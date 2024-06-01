1e7a1e1950da0a3ab011eb8b334bc316db8c2d1a
Nikita <jimolostlal@gmail.com>
2024-06-01 13:39:28 +0300

    SQL bank database

--- select.go
+++ select.go
@@ -1,18 +1,21 @@
 package main
+
 import (
+	"bufio"
+	"fmt"
 	"strconv"
-    "bufio"
-    "fmt"
+
 	//"reflect"
-    "log"
-    "os"
-    "net/http"
-    "database/sql"
-    _ "github.com/go-sql-driver/mysql"
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-    db, err := sql.Open("mysql", "root:@/test")
+    db, err := sql.Open("mysql", "nikita:5463@/bank")
      
     if err != nil {
         panic(err)
@@ -28,14 +31,22 @@ func main() {
 	// сохранение отправленных значений через поля формы.
 	http.HandleFunc("/postform", func(w http.ResponseWriter, r *http.Request){
      
-        val1 := r.FormValue("col1")
-        val2 := r.FormValue("col2")
-        val3 := r.FormValue("col3")
-		sQuery := "INSERT INTO myarttable (text, description, keywords) VALUES ('"+val1+"', '"+val2+"', '"+val3+"')"
+        firstName := r.FormValue("first_name")
+		lastName := r.FormValue("last_name")
+		patronymic := r.FormValue("patronymic")
+		passport := r.FormValue("passport")
+		tin := r.FormValue("tin")
+		snils := r.FormValue("snils")
+		driverLicense := r.FormValue("driver_license")
+		additionalDocuments := r.FormValue("additional_documents")
+		notes := r.FormValue("notes")
+		borrowerId := r.FormValue("borrower_id")
+
+		sQuery := "INSERT INTO individuals (first_name, last_name, patronymic, passport, tin, snils, driver_license, additional_documents, notes, borrower_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
  
 		fmt.Println(sQuery)
  
-		rows, err := db.Query(sQuery)
+		rows, err := db.Query(sQuery, firstName, lastName, patronymic, passport, tin, snils, driverLicense, additionalDocuments, notes, borrowerId)
 		if err != nil {
 			panic(err)
 		}		
@@ -62,8 +73,7 @@ func viewHeadQuery(w http.ResponseWriter, db *sql.DB, sShow string) {
     defer rows.Close()
 
 	fmt.Fprintf(w, "<tr>")
-	i := 0
-     for i < 4 {
+     for i := 0; i < 11; i++ {
 		rows.Next()
         p := sHead{}
         err := rows.Scan(&p.clnme)
@@ -72,21 +82,26 @@ func viewHeadQuery(w http.ResponseWriter, db *sql.DB, sShow string) {
             continue
         }
 		fmt.Fprintf(w, "<td>"+p.clnme+"</td>")
-		i++
     }
 	fmt.Fprintf(w, "</tr>")
 }
 
 // отправка в браузер строк из таблицы.
 func viewSelectQuery(w http.ResponseWriter, db *sql.DB, sSelect string) {
-	type test struct {
+	type individual struct {
 		id int
-		text string
-		description string
-		keywords string
+		firstName string
+		lastName string
+		patronymic string
+		passport string
+		tin string
+		snils string
+		driverLicense string
+		additionalDocuments string
+		notes string
+		borrowerId sql.NullInt64
 	}
-	tests := []test{}
-	//fmt.Println(reflect.TypeOf(tests))
+	individuals := []individual{}
 
 	// получение значений в массив tests из струкрур типа test.
     rows, err := db.Query(sSelect)
@@ -95,19 +110,19 @@ func viewSelectQuery(w http.ResponseWriter, db *sql.DB, sSelect string) {
     }
     defer rows.Close()
      
-    for rows.Next(){
-        p := test{}
-        err := rows.Scan(&p.id, &p.text, &p.description, &p.keywords)
+    for rows.Next() {
+        p := individual{}
+        err := rows.Scan(&p.id, &p.firstName, &p.lastName, &p.patronymic, &p.passport, &p.tin, &p.snils, &p.driverLicense, &p.additionalDocuments, &p.notes, &p.borrowerId)
         if err != nil{
             fmt.Println(err)
             continue
         }
-        tests = append(tests, p)
+        individuals = append(individuals, p)
     }
 	
 	// перебор массива из БД.
-	for _, p := range tests {
-		fmt.Fprintf(w, "<tr><td>"+strconv.Itoa(p.id)+"</td><td>"+p.text+"</td><td>"+p.description+"</td><td>"+p.keywords+"</td></tr>")
+	for _, p := range individuals {
+		fmt.Fprintf(w, "<tr><td>"+strconv.Itoa(p.id)+"</td><td>"+p.firstName+"</td><td>"+p.lastName+"</td><td>"+p.patronymic+"</td><td>"+p.passport+"</td><td>"+p.tin+"</td><td>"+p.snils+"</td><td>"+p.driverLicense+"</td><td>"+p.additionalDocuments+"</td><td>"+p.notes+"</td><td>"+strconv.FormatInt(p.borrowerId.Int64, 10)+"</td></tr>")
 	}
 }
 	
@@ -149,8 +164,8 @@ func viewSelect(w http.ResponseWriter, db *sql.DB) {
 			fmt.Fprintf(w, scanner.Text())
 		}
 		if scanner.Text() == "@tr" {
-			viewHeadQuery(w, db, "select COLUMN_NAME AS clnme from information_schema.COLUMNS where TABLE_NAME='myarttable'")
-			viewSelectQuery(w, db, "SELECT * FROM myarttable WHERE id>14 ORDER BY id DESC")
+			viewHeadQuery(w, db, "select COLUMN_NAME AS clnme from information_schema.COLUMNS where TABLE_NAME='individuals' ORDER BY ORDINAL_POSITION")
+			viewSelectQuery(w, db, "SELECT * FROM individuals ORDER BY id ASC")
 		}
 		if scanner.Text() == "@ver" {
 			viewSelectVerQuery(w, db, "SELECT VERSION() AS ver")
